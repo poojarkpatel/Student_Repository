fc80a578a37d9ae0079c6f05f8580332a71e0a2a
Pooja Patel <poojarkpatel0397@gmail.com>
2020-04-14 02:09:56 -0400

    HW11 commit

--- Student_Repository_Test_Pooja_Patel.py
+++ Student_Repository_Test_Pooja_Patel.py
@@ -7,39 +7,49 @@ class TestRepository(unittest.TestCase):
     def test_student_pretty_table(self):
         """ The function helps to test student_pretty_table function"""
 
-        stevens: Repository = Repository("/Users/poojapatel/PycharmProjects/HW10/stevens")
-        expected:List = [['10103', 'Baldwin, C', ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.44],
-                        ['10115', 'Wyatt, X', ['CS 545', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.81],
-                        ['10172', 'Forbes, I', ['SSW 555', 'SSW 567'], ['SSW 540', 'SSW 564'], ['CS 501', 'CS 513', 'CS 545'], 3.88],
-                        ['10175', 'Erickson, D', ['SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], ['CS 501', 'CS 513', 'CS 545'], 3.58],
-                        ['10183', 'Chapman, O', ['SSW 689'], ['SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'], ['CS 501', 'CS 513', 'CS 545'], 4.0],
-                        ['11399', 'Cordova, I', ['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], [], 3.0],
-                        ['11461', 'Wright, U', ['SYS 611', 'SYS 750', 'SYS 800'], ['SYS 612', 'SYS 671'], ['SSW 540', 'SSW 565', 'SSW 810'], 3.92],
-                        ['11658', 'Kelly, P', [], ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], 0.0],
-                        ['11714', 'Morton, A', ['SYS 611', 'SYS 645'], ['SYS 612', 'SYS 671', 'SYS 800'], ['SSW 540', 'SSW 565', 'SSW 810'], 3.0],
-                        ['11788', 'Fuller, E', ['SSW 540'], ['SYS 612', 'SYS 671', 'SYS 800'], [], 4.0]]
+        stevens: Repository = Repository("/Users/poojapatel/PycharmProjects/Student_Repository/stevens",
+                                         "/Applications/DataGrip.app/Contents/bin/Student_Repository.db")
+        expected: List = [['10103', 'Jobs, S', ['CS 501', 'SSW 810'], ['SSW 540', 'SSW 555'], [], 3.38],
+                          ['10115', 'Bezos, J', ['SSW 810'], ['SSW 540', 'SSW 555'],  ['CS 501', 'CS 546'], 4.0],
+                          ['10183', 'Musk, E', ['SSW 555', 'SSW 810'], ['SSW 540'],['CS 501', 'CS 546'], 4.0],
+                          ['11714', 'Gates, B', ['CS 546', 'CS 570', 'SSW 810'], [], [], 3.5]]
         calculated = [student.info() for cwid, student in stevens._students.items()]
-        self.assertEqual(calculated,expected)
+        self.assertEqual(calculated, expected)
 
     def test_instructor_pretty_table(self):
         """ The function helps to test instructor_pretty_table function"""
-        stevens: Repository = Repository("/Users/poojapatel/PycharmProjects/HW10/stevens")
+        stevens: Repository = Repository("/Users/poojapatel/PycharmProjects/Student_Repository/stevens",
+                                         "/Applications/DataGrip.app/Contents/bin/Student_Repository.db")
 
-        expected:Dict = {'SSW 540': {'cwid': '98765', 'Name': 'Einstein, A', 'Dept': 'SFEN', 'Students': 3},
-        'CS 545': {'cwid': '98764', 'Name': 'Feynman, R', 'Dept': 'SFEN', 'Students': 1},
-        'SSW 689': {'cwid': '98763', 'Name': 'Newton, I', 'Dept': 'SFEN', 'Students': 1},
-        'SYS 645': {'cwid': '98760', 'Name': 'Darwin, C', 'Dept': 'SYEN', 'Students': 1}}
-        self.assertEqual(stevens.instructor_pretty_table(),expected)
+        expected: Dict = {'CS 546': {'cwid': '98764', 'Name': 'Cohen, R', 'Dept': 'SFEN', 'Students': 1},
+                         'SSW 555': {'cwid': '98763', 'Name': 'Rowland, J', 'Dept': 'SFEN', 'Students': 1},
+                         'CS 570': {'cwid': '98762', 'Name': 'Hawking, S', 'Dept': 'CS', 'Students': 1}}
+        self.assertEqual(stevens.instructor_pretty_table(), expected)
 
     def test_majors_pretty_table(self):
         """ The function helps to test majors_pretty_table function"""
-        stevens: Repository = Repository("/Users/poojapatel/PycharmProjects/HW10/stevens")
-        expected:List =[['SFEN', ['SSW 540', 'SSW 555', 'SSW 564', 'SSW 567'],
-                         ['CS 501', 'CS 513', 'CS 545']],
-                        ['SYEN', ['SYS 612', 'SYS 671', 'SYS 800'],
-                         ['SSW 540', 'SSW 565', 'SSW 810']]]
+        stevens: Repository = Repository("/Users/poojapatel/PycharmProjects/Student_Repository/stevens",
+                                         "/Applications/DataGrip.app/Contents/bin/Student_Repository.db")
+        expected: List =[['CS', ['CS 546', 'CS 570'], ['SSW 565', 'SSW 810']],
+                        ['SFEN', ['SSW 540', 'SSW 555', 'SSW 810'], ['CS 501', 'CS 546']]]
         calculated = stevens.majors_pretty_table()
         self.assertEqual(calculated,expected)
+
+    def test_new_student_grades_table_db(self,):
+        """The function tests new_student_grades_table_db function"""
+        stevens: Repository = Repository("/Users/poojapatel/PycharmProjects/Student_Repository/stevens",
+                                         "/Applications/DataGrip.app/Contents/bin/Student_Repository.db")
+        expected: List = [('Bezos, J', '10115', 'SSW 810', 'A', 'Rowland, J'),
+                          ('Bezos, J', '10115', 'CS 546', 'F', 'Hawking, S'),
+                          ('Gates, B', '11714', 'SSW 810', 'B-', 'Rowland, J'),
+                          ('Gates, B', '11714', 'CS 546', 'A', 'Cohen, R'),
+                          ('Gates, B', '11714', 'CS 570', 'A-', 'Hawking, S'),
+                          ('Jobs, S', '10103', 'SSW 810', 'A-', 'Rowland, J'),
+                          ('Jobs, S', '10103', 'CS 501', 'B', 'Hawking, S'),
+                          ('Musk, E', '10183', 'SSW 555', 'A', 'Rowland, J'),
+                          ('Musk, E', '10183', 'SSW 810', 'A', 'Rowland, J')]
+        calculated = stevens.new_student_grades_table_db("/Applications/DataGrip.app/Contents/bin/Student_Repository.db")
+        self.assertEqual(expected,calculated)
 
 if __name__ == "__main__":
     unittest.main(exit=False,verbosity=2)
